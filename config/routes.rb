84904938280a829d9231f77b2e3172756df327b6
MOINTE <kevin.ran@live.cn>
2014-03-06 22:44:03 +0800

    修改主页无法滚动bug
    在container外层添加contents
    添加登陆注册链接

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ SoZo::Application.routes.draw do
 
   devise_for :users
   get 'home/index'
-  get 'home/login'
+  get 'users/sign_in'
+  get 'users/sign_up'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
